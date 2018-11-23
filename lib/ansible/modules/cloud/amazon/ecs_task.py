6460b726c7bd52393bc9dd575648da140e6b9c8a
Tad M <wm284@cornell.edu>
2018-11-23 16:23:54 -0500

    fix documentation for running ecs task on fargate

--- lib/ansible/modules/cloud/amazon/ecs_task.py
+++ lib/ansible/modules/cloud/amazon/ecs_task.py
@@ -101,20 +101,13 @@ EXAMPLES = '''
         - my_security_group
   register: task_output
 
-- name: RUN a task on Fargate
+
+- name: Start a task on Fargate
   ecs_task:
       operation: run
       cluster: console-sample-app-static-cluster
       task_definition: console-sample-app-static-taskdef
       task: "arn:aws:ecs:us-west-2:172139249013:task/3f8353d1-29a8-4689-bbf6-ad79937ffe8a"
-- name: Start a task on Fargate
-  ecs_task:
-      operation: start
-      cluster: console-sample-app-static-cluster
-      task_definition: console-sample-app-static-taskdef
-      task: "arn:aws:ecs:us-west-2:172139249013:task/3f8353d1-29a8-4689-bbf6-ad79937ffe8a"
-      container_instances:
-      - arn:aws:ecs:us-west-2:172139249013:container-instance/79c23f22-876c-438a-bddf-55c98a3538a8
       started_by: ansible_user
       launch_type: FARGATE
       network_configuration:
